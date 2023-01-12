b85fb6a3b7a46180862fd9fe399d2d2173922c4e
Daniel-17 <frimudaniel17@gmail.com>
2023-01-12 20:41:29 +0100

    component styles

--- 01_the_basics/src/app/app.component.ts
+++ 01_the_basics/src/app/app.component.ts
@@ -3,7 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css'],
+  styles: [
+    `
+      h3 {
+        color: #fff;
+      }
+    `,
+  ],
 })
 export class AppComponent {
   title = '01_the_basics';
