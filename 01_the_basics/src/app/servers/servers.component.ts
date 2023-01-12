aa4586ee675b78ab7823d9e191ab6aa4605aa053
Daniel-17 <frimudaniel17@gmail.com>
2023-01-12 20:36:40 +0100

    component templates

--- 01_the_basics/src/app/servers/servers.component.ts
+++ 01_the_basics/src/app/servers/servers.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  template: `
+    <app-server></app-server>
+    <app-server></app-server>
+  `,
+  styleUrls: ['./servers.component.css'],
 })
-export class ServersComponent {
-
-}
+export class ServersComponent {}
