cb6c2ae4a42950f600124848021563fd084328be
Daniel-17 <frimudaniel17@gmail.com>
2023-01-12 21:59:37 +0100

    property binding

--- 01_the_basics/src/app/servers/servers.component.ts
+++ 01_the_basics/src/app/servers/servers.component.ts
@@ -4,10 +4,17 @@ import { Component } from '@angular/core';
   selector: 'app-servers',
   // selector: '[app-servers]',
   // selector: '.app-servers',
-  template: `
-    <app-server></app-server>
-    <app-server></app-server>
-  `,
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>
+  // `,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css'],
 })
-export class ServersComponent {}
+export class ServersComponent {
+  allowNewServer = false;
+
+  constructor() {
+    setTimeout(() => (this.allowNewServer = true), 2000);
+  }
+}
