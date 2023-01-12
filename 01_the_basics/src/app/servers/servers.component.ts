37e7e82ea0b6ebeeb12b0d3a2e059cf18f51302d
Daniel-17 <frimudaniel17@gmail.com>
2023-01-12 21:26:01 +0100

    the component selector

--- 01_the_basics/src/app/servers/servers.component.ts
+++ 01_the_basics/src/app/servers/servers.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
+  // selector: '[app-servers]',
+  // selector: '.app-servers',
   template: `
     <app-server></app-server>
     <app-server></app-server>
