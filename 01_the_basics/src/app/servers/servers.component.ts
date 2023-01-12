fd13427cd79eb56d4d9904583e225e7d418028eb
Daniel-17 <frimudaniel17@gmail.com>
2023-01-12 22:05:39 +0100

    event binding

--- 01_the_basics/src/app/servers/servers.component.ts
+++ 01_the_basics/src/app/servers/servers.component.ts
@@ -13,8 +13,13 @@ import { Component } from '@angular/core';
 })
 export class ServersComponent {
   allowNewServer = false;
+  serverCreationStatus = 'No server was created.';
 
   constructor() {
     setTimeout(() => (this.allowNewServer = true), 2000);
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created.';
   }
 }
