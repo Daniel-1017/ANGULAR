421b248e553cce852aa584974325b63da43b1c44
Daniel-17 <frimudaniel17@gmail.com>
2023-01-12 20:34:13 +0100

    creating components with the CLI & nesting components

--- 01_the_basics/src/app/server/server.component.ts
+++ 01_the_basics/src/app/server/server.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  template: './server.component.html',
+  templateUrl: './server.component.html',
 })
 export class ServerComponent {}
