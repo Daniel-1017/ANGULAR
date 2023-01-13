f1318990473befaca8572759457e5c91e7a1e65e
Daniel-17 <frimudaniel17@gmail.com>
2023-01-13 18:51:17 +0100

    @ViewChild

--- 03_components_and_databinding/src/app/cockpit/cockpit.component.ts
+++ 03_components_and_databinding/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,11 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -15,7 +22,9 @@ export class CockpitComponent implements OnInit {
     serverContent: string;
   }>();
   // newServerName = '';
-  newServerContent = '';
+  // newServerContent = '';
+  @ViewChild('serverContentInput', { static: true })
+  serverContentInput: ElementRef;
 
   constructor() {}
 
@@ -24,14 +33,14 @@ export class CockpitComponent implements OnInit {
   onAddServer(nameInput: HTMLInputElement) {
     this.serverCreated.emit({
       serverName: nameInput.value,
-      serverContent: this.newServerContent,
+      serverContent: this.serverContentInput.nativeElement.value,
     });
   }
 
   onAddBlueprint(nameInput: HTMLInputElement) {
     this.blueprintCreated.emit({
       serverName: nameInput.value,
-      serverContent: this.newServerContent,
+      serverContent: this.serverContentInput.nativeElement.value,
     });
   }
 }
