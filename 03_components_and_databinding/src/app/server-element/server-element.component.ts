e5146660ecf2b1c705de951f80eee8a1b50650a5
Daniel-17 <frimudaniel17@gmail.com>
2023-01-13 19:40:25 +0100

    lifecycle hooks

--- 03_components_and_databinding/src/app/server-element/server-element.component.ts
+++ 03_components_and_databinding/src/app/server-element/server-element.component.ts
@@ -1,14 +1,70 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+  OnChanges,
+  SimpleChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy,
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css'],
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent
+  implements
+    OnInit,
+    OnChanges,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterContentChecked,
+    AfterViewChecked,
+    OnDestroy
+{
   @Input('srvElement') element: { type: string; name: string; content: string };
 
-  constructor() {}
+  constructor() {
+    console.log('constructor called');
+  }
 
-  ngOnInit(): void {}
+  ngOnChanges(changes: SimpleChanges) {
+    console.log('%cngOnChanges called', 'color: green');
+    // console.log(changes);
+  }
+
+  ngOnInit() {
+    console.log('%cngOnInit called', 'color: red');
+  }
+
+  ngDoCheck() {
+    console.log('%cngDoCheck is called', 'color: yellow');
+  }
+
+  ngAfterContentInit() {
+    console.log('%cngAfterContentInit is called', 'color: orange');
+  }
+
+  ngAfterContentChecked() {
+    console.log('%cngAfterContentChecked is called', 'color: lightblue');
+  }
+
+  ngAfterViewInit() {
+    console.log('%cngAfterViewInit is called', 'color: cyan');
+  }
+
+  ngAfterViewChecked() {
+    console.log('%cngAfterViewChecked is called', 'color: purple');
+  }
+
+  ngOnDestroy() {
+    console.log('%cngOnDestroy is called', 'color: lightgreen');
+  }
 }
