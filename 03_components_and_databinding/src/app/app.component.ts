4cd2e37c8933b8b93652fa3529d9671562daa9f6
Daniel-17 <frimudaniel17@gmail.com>
2023-01-13 18:12:24 +0100

    splitting apps in components & binding to custom properies

--- 03_components_and_databinding/src/app/app.component.ts
+++ 03_components_and_databinding/src/app/app.component.ts
@@ -3,26 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  serverElements = [];
-  newServerName = '';
-  newServerContent = '';
-
-  onAddServer() {
-    this.serverElements.push({
-      type: 'server',
-      name: this.newServerName,
-      content: this.newServerContent
-    });
-  }
-
-  onAddBlueprint() {
-    this.serverElements.push({
-      type: 'blueprint',
-      name: this.newServerName,
-      content: this.newServerContent
-    });
-  }
+  serverElements = [
+    { type: 'server', name: 'Testserver', content: 'Just a test' },
+  ];
 }
