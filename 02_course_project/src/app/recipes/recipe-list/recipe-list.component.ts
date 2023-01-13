d22bdef3c806870d574f61aa5644b1bfbaa455fd
Daniel-17 <frimudaniel17@gmail.com>
2023-01-13 16:20:04 +0100

    recipe model

--- 02_course_project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ 02_course_project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
-
+  recipes = [];
 }
