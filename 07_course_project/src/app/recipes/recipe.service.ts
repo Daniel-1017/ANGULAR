47bf5eab19f066098e6ea6b6f06c59313f941f67
Daniel-17 <frimudaniel17@gmail.com>
2023-01-16 17:55:11 +0100

    managing recipes in recipe service

--- 07_course_project/src/app/recipes/recipe.service.ts
+++ 07_course_project/src/app/recipes/recipe.service.ts
@@ -1,3 +1,20 @@
+import { Recipe } from './recipe.model';
+
 export class RecipeService {
-  // to do
+  private recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
+    ),
+    new Recipe(
+      'Another Test Recipe',
+      'This is simply a test',
+      'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'
+    ),
+  ];
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
 }
