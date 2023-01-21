1672b7a81e94912fb46f493bb0ba7ace1435d65f
Daniel-17 <frimudaniel17@gmail.com>
2023-01-21 12:26:04 +0100

    configuring the handling of query params

--- 08_changing_pages_with_routing/src/app/servers/server/server.component.ts
+++ 08_changing_pages_with_routing/src/app/servers/server/server.component.ts
@@ -26,6 +26,9 @@ export class ServerComponent implements OnInit {
   }
 
   onEdit() {
-    this.router.navigate(['edit']);
+    this.router.navigate(['edit'], {
+      relativeTo: this.route,
+      queryParamsHandling: 'preserve',
+    });
   }
 }
