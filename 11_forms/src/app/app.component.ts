06bbc2bacf65f24c4c0f246e239f259cbdec7675
Daniel-17 <frimudaniel17@gmail.com>
2023-01-25 18:39:18 +0100

    TD: handling radio buttons

--- 11_forms/src/app/app.component.ts
+++ 11_forms/src/app/app.component.ts
@@ -9,7 +9,8 @@ import { NgForm } from '@angular/forms';
 export class AppComponent {
   @ViewChild('f') form: NgForm;
   defaultQuestion = 'pet';
-  answer: '';
+  answer = '';
+  genders = ['male', 'female'];
 
   suggestUserName() {
     const suggestedName = 'Superuser';
