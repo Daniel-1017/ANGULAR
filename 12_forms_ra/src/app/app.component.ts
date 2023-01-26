a7f63d6d6e70208145f0aace1c43271decf41afe
Daniel-17 <frimudaniel17@gmail.com>
2023-01-26 19:39:56 +0100

    RA: reacting to status or value changes

--- 12_forms_ra/src/app/app.component.ts
+++ 12_forms_ra/src/app/app.component.ts
@@ -38,6 +38,8 @@ export class AppComponent implements OnInit {
       gender: new FormControl('male'),
       hobbies: new FormArray([]),
     });
+    // this.signupForm.valueChanges.subscribe((value) => console.log(value));
+    // this.signupForm.statusChanges .subscribe((value) => console.log(value));
   }
 
   onSubmit() {
