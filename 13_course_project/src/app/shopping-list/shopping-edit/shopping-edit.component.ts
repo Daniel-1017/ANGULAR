ed20ce2f38466f0fbb33320ca24072515c34deb6
Daniel-17 <frimudaniel17@gmail.com>
2023-01-27 18:17:35 +0100

    resetting form

--- 13_course_project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ 13_course_project/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -33,7 +33,7 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     );
   }
 
-  onAddItem(form: NgForm) {
+  onSubmit(form: NgForm) {
     const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
     if (this.editMode) {
@@ -41,6 +41,8 @@ export class ShoppingEditComponent implements OnInit, OnDestroy {
     } else {
       this.slService.addIngredient(newIngredient);
     }
+    this.editMode = false;
+    form.reset();
   }
 
   ngOnDestroy() {
