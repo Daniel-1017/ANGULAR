520b57428703c06a679801a8efa550003a04c29c
Daniel-17 <frimudaniel17@gmail.com>
2023-01-31 19:56:49 +0100

    showing a loader indicator

--- 15_http_requests/src/app/app.component.ts
+++ 15_http_requests/src/app/app.component.ts
@@ -10,6 +10,7 @@ import { Post } from './post.model';
 })
 export class AppComponent implements OnInit {
   loadedPosts: Post[] = [];
+  isFetching = false;
 
   constructor(private http: HttpClient) {}
 
@@ -39,6 +40,7 @@ export class AppComponent implements OnInit {
   }
 
   private fetchPosts() {
+    this.isFetching = true;
     this.http
       .get<{ [key: string]: Post }>(
         'https://ng-complete-guide-a1e66-default-rtdb.firebaseio.com/posts.json'
@@ -55,6 +57,7 @@ export class AppComponent implements OnInit {
         })
       )
       .subscribe((posts) => {
+        this.isFetching = false;
         this.loadedPosts = posts;
       });
   }
