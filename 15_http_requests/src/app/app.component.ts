2d9f0566ac01e3f3a5fea92c67dcd51036730e75
Daniel-17 <frimudaniel17@gmail.com>
2023-01-31 19:17:01 +0100

    getting data

--- 15_http_requests/src/app/app.component.ts
+++ 15_http_requests/src/app/app.component.ts
@@ -11,7 +11,9 @@ export class AppComponent implements OnInit {
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.fetchPosts();
+  }
 
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
@@ -27,9 +29,20 @@ export class AppComponent implements OnInit {
 
   onFetchPosts() {
     // Send Http request
+    this.fetchPosts();
   }
 
   onClearPosts() {
     // Send Http request
+  }
+
+  private fetchPosts() {
+    this.http
+      .get(
+        'https://ng-complete-guide-a1e66-default-rtdb.firebaseio.com/posts.json'
+      )
+      .subscribe((posts) => {
+        console.log(posts);
+      });
   }
 }
