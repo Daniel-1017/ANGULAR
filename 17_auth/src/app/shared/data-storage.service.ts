4838a58fc85e3d89e2b169c2ebe5d633a8599ad3
Daniel-17 <frimudaniel17@gmail.com>
2023-02-03 18:19:18 +0100

    -- 17 auth --

---
+++ 17_auth/src/app/shared/data-storage.service.ts
@@ -0,0 +1,42 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map, tap } from 'rxjs/operators';
+import { Recipe } from '../recipes/recipe.model';
+import { RecipeService } from '../recipes/recipe.service';
+
+@Injectable({ providedIn: 'root' })
+export class DataStorageService {
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    this.http
+      .put(
+        'https://ng-complete-guide-a1e66-default-rtdb.firebaseio.com/recipes.json',
+        recipes
+      )
+      .subscribe((res) => {
+        console.log(res);
+      });
+  }
+
+  fetchRecipes() {
+    return this.http
+      .get<Recipe[]>(
+        'https://ng-complete-guide-a1e66-default-rtdb.firebaseio.com/recipes.json'
+      )
+      .pipe(
+        map((recipes) => {
+          return recipes.map((recipe) => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : [],
+            };
+          });
+        }),
+        tap((recipes) => {
+          this.recipeService.setRecipes(recipes);
+        })
+      );
+  }
+}
