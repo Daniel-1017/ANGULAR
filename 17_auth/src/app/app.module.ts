3bc7fa5d1991a2d3e160c9dd0527f3fa36f4ad85
Daniel-17 <frimudaniel17@gmail.com>
2023-02-03 18:28:38 +0100

    adding the auth page

--- 17_auth/src/app/app.module.ts
+++ 17_auth/src/app/app.module.ts
@@ -17,6 +17,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeService } from './recipes/recipe.service';
+import { AuthComponent } from './auth/auth.component';
 
 @NgModule({
   declarations: [
@@ -31,6 +32,7 @@ import { RecipeService } from './recipes/recipe.service';
     DropdownDirective,
     RecipeStartComponent,
     RecipeEditComponent,
+    AuthComponent,
   ],
   imports: [
     BrowserModule,
